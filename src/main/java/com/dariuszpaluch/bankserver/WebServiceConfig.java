3a41b6a23af04174b79417c5e360bfe351a4c706
Dariusz Paluch <dariusz@dariuszpaluch.com>
2018-01-06 11:28:51 +0100

    Create example country soap service

---
+++ src/main/java/com/dariuszpaluch/bankserver/WebServiceConfig.java
@@ -0,0 +1,43 @@
+package com.dariuszpaluch.bankserver;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+/**
+ * Created by Dariusz Paluch on 06.01.2018.
+ */
+@EnableWs
+@Configuration
+public class WebServiceConfig {
+
+  @Bean
+  public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+    MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+    servlet.setApplicationContext(applicationContext);
+    servlet.setTransformWsdlLocations(true);
+    return new ServletRegistrationBean(servlet, "/ws/*");
+  }
+
+  @Bean(name = "bank")
+  public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
+    DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+    wsdl11Definition.setPortTypeName("BankPort");
+    wsdl11Definition.setLocationUri("/ws");
+    wsdl11Definition.setTargetNamespace("http://spring.io/guides/gs-producing-web-service");
+    wsdl11Definition.setSchema(countriesSchema);
+    return wsdl11Definition;
+  }
+
+  @Bean
+  public XsdSchema countriesSchema() {
+    return new SimpleXsdSchema(new ClassPathResource("bank.xsd"));
+  }
+}
