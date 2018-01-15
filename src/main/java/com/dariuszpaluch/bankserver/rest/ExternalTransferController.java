42dc29cd12d5d55d678ab109fe2f600c45f96433
Dariusz Paluch <dariusz@dariuszpaluch.com>
2018-01-15 22:59:04 +0100

    Change ExternalTransferRequest

--- src/main/java/com/dariuszpaluch/bankserver/rest/ExternalTransferController.java
+++ src/main/java/com/dariuszpaluch/bankserver/rest/ExternalTransferController.java
@@ -3,8 +3,10 @@ package com.dariuszpaluch.bankserver.rest;
 import com.dariuszpaluch.bankserver.BankDAO;
 import com.dariuszpaluch.bankserver.exceptions.AccountNumberDoesNotExist;
 import com.dariuszpaluch.bankserver.exceptions.DatabaseException;
+import com.dariuszpaluch.bankserver.exceptions.IncorrectAmount;
 import com.dariuszpaluch.bankserver.models.ExternalTransferRequest;
 import com.dariuszpaluch.bankserver.utils.BankAccountUtils;
+import com.dariuszpaluch.bankserver.utils.ValidationsUtils;
 import com.dariuszpaluch.services.bank.Transfer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,14 +24,19 @@ public class ExternalTransferController {
   private BankDAO bankDAO = BankDAO.getInstance();
 
   @RequestMapping(value = "/{destinationAccount}/history", method = RequestMethod.POST)
-  public ResponseEntity transfer(@Valid @RequestBody ExternalTransferRequest externalIncomingTransferRequest, @PathVariable("destinationAccount") String destinationAccount) {
+  public ResponseEntity transfer(@Valid @RequestBody ExternalTransferRequest request, @PathVariable("destinationAccount") String destinationAccount) {
 
+    try {
+      ValidationsUtils.verificationAmount(request.getAmount());
+    } catch (IncorrectAmount incorrectAmount) {
+      return ResponseEntity.badRequest().body(new ValidationError("amount", "Wrong amount value."));
+    }
     System.out.println("GET EXTERNAL TRANSFER");
     if (!BankAccountUtils.validateIBAN(destinationAccount)) {
       return ResponseEntity.badRequest().body(new ValidationError("url destinationAccount", "Is not validate IBAN"));
     }
 
-    if (!BankAccountUtils.validateIBAN(externalIncomingTransferRequest.getSource_account())) {
+    if (!BankAccountUtils.validateIBAN(request.getSource_account())) {
       return ResponseEntity.badRequest().body(new ValidationError("source_account", "Is not validate IBAN"));
     }
 
@@ -39,11 +46,11 @@ public class ExternalTransferController {
 
     //TODO implement save in database
     Transfer transfer = new Transfer();
-    transfer.setAmount(externalIncomingTransferRequest.getAmount());
-    transfer.setSourceAccount(externalIncomingTransferRequest.getSource_account());
+    transfer.setAmount(request.getAmount());
+    transfer.setSourceAccount(request.getSource_account());
     transfer.setDestinationAccount(destinationAccount);
-    transfer.setTitle(externalIncomingTransferRequest.getTitle());
-    transfer.setName(externalIncomingTransferRequest.getName());
+    transfer.setTitle(request.getTitle());
+    transfer.setName(request.getName());
 
     try {
       this.bankDAO.externalIncomingTransfer(transfer);
